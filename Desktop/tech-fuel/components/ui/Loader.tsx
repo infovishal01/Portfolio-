ff11cfdebfae556ff66ea69335faa055d4e5f39c
Vishal Kumar <106486681+infovishal01@users.noreply.github.com>
2026-05-08 07:14:30 +0530

    Delete Desktop/tech-fuel directory

--- Desktop/tech-fuel/components/ui/Loader.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loader() {
-  return (
-    <div className="flex justify-center items-center py-20">
-
-      <div className="w-12 h-12 border-4 border-zinc-800 border-t-green-500 rounded-full animate-spin" />
-
-    </div>
-  );
-}
